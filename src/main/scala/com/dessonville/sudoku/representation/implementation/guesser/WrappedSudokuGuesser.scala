6516e149a8cb007a4fcae604c5966b2be61a0310
Chris Dessonville <chris@chris-macbook.local>
2013-07-20 21:51:15 -0700

    WrappedSudokuGuesser to wrap a sudoku

---
+++ src/main/scala/com/dessonville/sudoku/representation/implementation/guesser/WrappedSudokuGuesser.scala
@@ -0,0 +1,23 @@
+package com.dessonville.sudoku.representation.implementation.guesser
+
+import com.dessonville.sudoku.representation.{Sudoku, SudokuGuesser}
+
+abstract class WrappedSudokuGuesser[R](private val wrapped: Sudoku[R]) extends SudokuGuesser[R] {
+  def allowedItems: Set[R] = wrapped.allowedItems
+
+  def emptyItem: R = wrapped.emptyItem
+
+  def innerDimension: Int = wrapped.innerDimension
+
+  def outerDimension: Int = wrapped.outerDimension
+
+  def getRow(row: Int): Iterable[R] = wrapped.getRow(row)
+
+  def getColumn(col: Int): Iterable[R] = wrapped.getColumn(col)
+
+  def getBox(col: Int, row: Int): Iterable[Iterable[R]] = wrapped.getBox(col, row)
+
+  def setValue(col: Int, row: Int, value: R)  = wrapped.setValue(col, row, value)
+
+  def getValue(col: Int, row: Int): R = wrapped.getValue(col, row)
+}
