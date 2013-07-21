aad66717dae454901591b9641f0dcf94536cc60b
Chris Dessonville <chris@chris-macbook.local>
2013-07-20 19:05:33 -0700

    SudokuGuesser trait.  Used to track information such as guessed sudokus

---
+++ src/main/scala/com/dessonville/sudoku/representation/SudokuGuesser.scala
@@ -0,0 +1,46 @@
+package com.dessonville.sudoku.representation
+
+/**
+ * Typically used to wrap a sudoku.  Used to record known information while analyzing a sudoku
+ */
+trait SudokuGuesser[R] extends Sudoku[R] {
+  /**
+   * Get the possibilities for a cell
+   * @param col
+   * @param row
+   * @return
+   */
+  def getPossibilities(col: Int, row: Int): Set[R]
+
+  /**
+   * Remove a set of possibilities from a cell
+   * @param col
+   * @param row
+   * @param values
+   */
+  def removePossibilities(col: Int, row: Int, values: Set[R]): Unit
+
+  /**
+   * Set the values for a cell and automatically removes the value from all items in the row, column, and box
+   * @param col
+   * @param row
+   * @param value
+   */
+  def setValueAndRemovePossibilities(col: Int, row: Int, value: R): Unit
+
+  /**
+   * Remove a single possibility from a cell
+   * @param col
+   * @param row
+   * @param value
+   */
+  def removePossibility(col: Int, row: Int, value: R) = removePossibilities(col, row, Set(value))
+
+  /**
+   * Is this cell's value already determined?
+   * @param col
+   * @param row
+   * @return
+   */
+  def isDetermined(col: Int, row: Int): Boolean = getPossibilities(col, row).size == 1
+}
